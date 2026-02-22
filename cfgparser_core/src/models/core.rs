6d190c1c77ae210f7bebece69934a7f567fa988f
thomas-osgood <thomas-osgood@users.noreply.github.com>
2026-02-22 18:55:39 -0500

    updated struct types

--- cfgparser_core/src/models/core.rs
+++ cfgparser_core/src/models/core.rs
@@ -1,11 +1,11 @@
 #[derive(serde::Deserialize, Debug)]
 pub struct Configuration {
-    pub host: Vec<u8>,
+    pub host: String,
     pub port: i8,
 }
 
 impl Configuration {
-    pub fn new(host: Vec<u8>, port: i8) -> Configuration {
+    pub fn new(host: String, port: i8) -> Configuration {
         Configuration {
             host: host,
             port: port,
