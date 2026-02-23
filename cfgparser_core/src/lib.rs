cdddf072b727843ca7c05e40ac9490251d31635c
thomas-osgood <thomas-osgood@users.noreply.github.com>
2026-02-22 19:11:39 -0500

    use transformer module

--- cfgparser_core/src/lib.rs
+++ cfgparser_core/src/lib.rs
@@ -2,6 +2,7 @@ use base64::{self, Engine};
 use cfgparser_encryption;
 
 mod models;
+mod transformer;
 
 #[no_mangle]
 /// function designed to read the configuration bytes and
@@ -18,7 +19,7 @@ pub extern "C" fn read_cfg() {
         cfgparser_encryption::xor::engine::encrypt_decrypt(key.to_vec(), msg.into());
 
     let decoded: String;
-    match transform_payload(key, &ciphertext) {
+    match transformer::core::transform_payload(key, &ciphertext) {
         Ok(result) => decoded = String::from_utf8_lossy(&result).to_string(),
         Err(e) => {
             println!("ERROR DECODING: {:#}", e);
@@ -28,13 +29,14 @@ pub extern "C" fn read_cfg() {
 
     println!("DECODED: {:#}", decoded);
 
-    let configuration: models::core::Configuration = match deserialize_payload(decoded) {
-        Ok(result) => result,
-        Err(e) => {
-            println!("ERROR DESERIALIZING JSON: {:#}", e);
-            return;
-        }
-    };
+    let configuration: models::core::Configuration =
+        match transformer::core::deserialize_payload(decoded) {
+            Ok(result) => result,
+            Err(e) => {
+                println!("ERROR DESERIALIZING JSON: {:#}", e);
+                return;
+            }
+        };
 
     println!("CONFIGURATION: {:#?}", configuration);
 
@@ -42,29 +44,8 @@ pub extern "C" fn read_cfg() {
     println!("ADDRESS: {:#}", address);
 }
 
-/// function designed to JSON deserialize a given string into
-/// a Configuration struct and return the struct.
-///
-/// if there is an error, a `serde_json::Error` will be returned.
-fn deserialize_payload(payload: String) -> Result<models::core::Configuration, serde_json::Error> {
-    match serde_json::from_str(&payload) {
-        Ok(result) => Ok(result),
-        Err(e) => Err(e),
-    }
-}
-
-/// function designed to convert the raw bytes read from
-/// the end of the file into a configuration.
-///
-/// process:
-///
-/// 1. decrypt bytes
-/// 2. base64-decode plaintext
-fn transform_payload(key: &[u8], raw_payload: &[u8]) -> Result<Vec<u8>, base64::DecodeError> {
-    // XOR decrypt the raw_payload bytes.
-    let plaintext: Vec<u8> =
-        cfgparser_encryption::xor::engine::encrypt_decrypt(key.to_vec(), raw_payload.to_vec());
-
-    // base64-decode the payload and save the result
-    base64::engine::general_purpose::STANDARD_NO_PAD.decode(plaintext)
-}
+/// function designed to read the end of the current binary
+/// and extract the configuration bytes. these bytes will then
+/// be processed in a later step to transform them into a
+/// Configuration struct to allow access to all necessary information.
+fn read_cfg_bytes() {}
