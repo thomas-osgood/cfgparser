f57da19f903d52ca8d84002140adc75d21c127c7
thomas-osgood <thomas-osgood@users.noreply.github.com>
2026-02-23 20:10:10 -0500

    fixed port type

--- cfgparser_core/src/models/core.rs
+++ cfgparser_core/src/models/core.rs
@@ -1,11 +1,11 @@
 #[derive(serde::Deserialize, Debug)]
 pub struct Configuration {
     pub host: String,
-    pub port: i8,
+    pub port: i64,
 }
 
 impl Configuration {
-    pub fn new(host: String, port: i8) -> Configuration {
+    pub fn new(host: String, port: i64) -> Configuration {
         Configuration {
             host: host,
             port: port,
