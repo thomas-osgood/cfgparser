94a197ee549adbc5d9306ae9179ae88ccc98df2b
thomas-osgood <thomas-osgood@users.noreply.github.com>
2026-02-26 01:05:31 -0500

    began unit tests for caesar module

---
+++ cfgparser_encryption/src/caesar/engine/unit_tests.rs
@@ -0,0 +1,31 @@
+use super::*;
+
+#[test]
+fn test_encrypt() {
+    let key: Vec<u8> = "there".to_string().into_bytes();
+    let plaintext: Vec<u8> = "hello everybody".to_string().into_bytes();
+
+    let expected: Vec<u8> = "alpcs xciicuvhp".to_string().into_bytes();
+    let result: Vec<u8> = encrypt(plaintext, key);
+
+    assert_eq!(result, expected);
+}
+
+#[test]
+fn test_isletter() {
+    assert_eq!(is_letter(b'a'), true);
+    assert_eq!(is_letter(b'z'), true);
+    assert_eq!(is_letter(b'A'), true);
+    assert_eq!(is_letter(b'Z'), true);
+    assert_eq!(is_letter(b' '), false);
+    assert_eq!(is_letter(b'-'), false);
+    assert_eq!(is_letter(b'0'), false);
+    assert_eq!(is_letter(b'9'), false);
+}
+
+#[test]
+fn test_rotate() {
+    assert_eq!(rotate(b'a', b'a'), b'a');
+    assert_eq!(rotate(b'm', b'c'), b'o');
+    assert_eq!(rotate(b'm', b'y'), b'k');
+}
