6659a7e8d591eeecb0448fec51da1a6c07f23e16
thomas-osgood <thomas-osgood@users.noreply.github.com>
2026-02-26 20:19:28 -0500

    began test_transform_payload

--- cfgparser_core/src/transformer/core/unit_tests.rs
+++ cfgparser_core/src/transformer/core/unit_tests.rs
@@ -17,3 +17,20 @@ fn test_deserialize_payload() {
 
     assert_eq!(is_equal, true);
 }
+
+#[test]
+fn test_transform_payload() {
+    let key: &str = "testkey";
+    let expected: String = "This is a decrypted message".to_string();
+    let message: [u8; 36] = [
+        34, 34, 27, 4, 8, 28, 59, 4, 6, 10, 54, 3, 44, 62, 38, 9, 42, 71, 33, 80, 26, 60, 55, 31,
+        46, 40, 39, 13, 46, 61, 61, 14, 50, 50, 29, 24,
+    ];
+
+    let result: Vec<u8> =
+        transform_payload(key.as_bytes(), &message).expect("error transforming payload");
+    let str_result: String =
+        String::from_utf8(result).expect("error transforming result to string");
+
+    assert_eq!(str_result, expected);
+}
