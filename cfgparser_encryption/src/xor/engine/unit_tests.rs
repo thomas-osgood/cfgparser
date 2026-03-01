38fa9ce47d2eeb651d8f5081e65405fde4191ac1
thomas-osgood <thomas-osgood@users.noreply.github.com>
2026-03-01 15:26:22 -0500

    added test_encrypt test

--- cfgparser_encryption/src/xor/engine/unit_tests.rs
+++ cfgparser_encryption/src/xor/engine/unit_tests.rs
@@ -13,3 +13,21 @@ fn test_encrypt_decrypt() {
 
     assert_eq!(result_full_cycle, test_message);
 }
+
+#[test]
+/// test designed to confirm the encrypt_decrypt function encrypts a message correctly.
+fn test_encrypt() {
+    let test_key: Vec<u8> = "msgpadkey".as_bytes().to_vec();
+    let test_msg: Vec<u8> = "this is my secret message. you cannot get the contents of it."
+        .as_bytes()
+        .to_vec();
+    let expected: Vec<u8> = vec![
+        25, 27, 14, 3, 65, 13, 24, 69, 20, 20, 83, 20, 21, 2, 22, 14, 17, 89, 0, 22, 20, 3, 0, 3,
+        14, 75, 89, 20, 28, 18, 80, 2, 5, 5, 11, 22, 25, 83, 0, 21, 21, 68, 31, 13, 28, 77, 16, 8,
+        30, 21, 1, 5, 17, 10, 77, 28, 1, 80, 8, 16, 69,
+    ];
+
+    let result: Vec<u8> = encrypt_decrypt(test_key, test_msg);
+
+    assert_eq!(result, expected);
+}
