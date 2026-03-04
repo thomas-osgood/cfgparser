b0ded4bab1ce74674f61004ee4cf3ee172d8d15c
thomas-osgood <thomas-osgood@users.noreply.github.com>
2026-03-03 20:09:31 -0500

    updated "read" test

--- cfgparser_core/src/unit_tests.rs
+++ cfgparser_core/src/unit_tests.rs
@@ -1,17 +1,26 @@
 use super::*;
 
 #[test]
+/// test designed to confirm the "read()" function operates as
+/// expected and returns the proper result.
 fn test_read() -> Result<(), Box<dyn std::error::Error>> {
     let expected: models::core::Configuration = models::core::Configuration {
         host: "secrethost".to_string(),
         port: 8000,
     };
+
+    let unexpected: models::core::Configuration = models::core::Configuration {
+        host: "secrethost22".to_string(),
+        port: 80,
+    };
+
     let key: &str = "secret";
     let reader: extractor::core::TestExtractor = extractor::core::TestExtractor;
 
     let extracted: models::core::Configuration = read(reader, key.as_bytes())?;
 
     assert_eq!(extracted, expected);
+    assert_ne!(extracted, unexpected);
 
     Ok(())
 }
