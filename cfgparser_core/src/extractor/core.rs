89524965697a5cf97928761a14c9d7f10d52691a
thomas-osgood <thomas-osgood@users.noreply.github.com>
2026-03-04 23:17:24 -0500

    added read_bytes  helper func

--- cfgparser_core/src/extractor/core.rs
+++ cfgparser_core/src/extractor/core.rs
@@ -13,6 +13,37 @@ pub trait CfgExtractor {
     fn extract_cfg_bytes(&self) -> std::io::Result<Vec<u8>>;
 }
 
+/// generic function designed to read the configuration bytes from
+/// a file and return either a Vec<u8> or an error.
+fn read_bytes(mut fptr: std::fs::File) -> std::io::Result<Vec<u8>> {
+    let size_start: i64 = SZ_SIZEBUFF as i64 * -1;
+
+    // allocate buffer that will hold the size bytes.
+    let mut buf_sz: [u8; SZ_SIZEBUFF as usize] = [0; SZ_SIZEBUFF];
+    // jumpt to the start of the size bytes (end - 8 bytes).
+    let _: u64 = fptr.seek(std::io::SeekFrom::End(size_start))?;
+
+    fptr.read_exact(&mut buf_sz)?;
+
+    // convert the bytes read into i64.
+    let sz_payload: i64 = i64::from_be_bytes(buf_sz);
+
+    // calculate how far back to go from the end of the file
+    // to reach the beginning byte of the config.
+    let cfg_offset: i64 = (8 + sz_payload) * -1;
+
+    // jump to the start of the config.
+    fptr.seek(std::io::SeekFrom::End(cfg_offset))?;
+
+    // allocate buffer for payload bytes.
+    let mut payload_buf: Vec<u8> = vec![0u8; sz_payload as usize];
+
+    // read the payload bytes into the payload buffer.
+    fptr.read_exact(&mut payload_buf)?;
+
+    Ok(payload_buf)
+}
+
 /// struct designed to extract configuration bytes from the
 /// current binary. this implements the CfgExtractor trait.
 pub struct SelfExtractor;
@@ -21,33 +52,9 @@ impl CfgExtractor for SelfExtractor {
     /// bytes from the current binary.
     fn extract_cfg_bytes(&self) -> std::io::Result<Vec<u8>> {
         let current_binary: std::path::PathBuf = std::env::current_exe()?;
-        let mut fptr: std::fs::File = std::fs::File::open(current_binary)?;
-        let size_start: i64 = SZ_SIZEBUFF as i64 * -1;
-
-        // allocate buffer that will hold the size bytes.
-        let mut buf_sz: [u8; SZ_SIZEBUFF as usize] = [0; SZ_SIZEBUFF];
-        // jumpt to the start of the size bytes (end - 8 bytes).
-        let _: u64 = fptr.seek(std::io::SeekFrom::End(size_start))?;
-
-        fptr.read_exact(&mut buf_sz)?;
-
-        // convert the bytes read into i64.
-        let sz_payload: i64 = i64::from_be_bytes(buf_sz);
-
-        // calculate how far back to go from the end of the file
-        // to reach the beginning byte of the config.
-        let cfg_offset: i64 = (8 + sz_payload) * -1;
-
-        // jump to the start of the config.
-        fptr.seek(std::io::SeekFrom::End(cfg_offset))?;
-
-        // allocate buffer for payload bytes.
-        let mut payload_buf: Vec<u8> = vec![0u8; sz_payload as usize];
-
-        // read the payload bytes into the payload buffer.
-        fptr.read_exact(&mut payload_buf)?;
-
-        Ok(payload_buf)
+        let fptr: std::fs::File = std::fs::File::open(current_binary)?;
+        // read and return the  bytes from the file.
+        read_bytes(fptr)
     }
 }
 
