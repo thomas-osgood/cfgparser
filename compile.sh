dd326f3188194f313fdf9a60c109a56cdca479c6
thomas-osgood <thomas-osgood@users.noreply.github.com>
2026-03-07 14:34:24 -0500

    made script more friendly

--- compile.sh
+++ compile.sh
@@ -7,5 +7,11 @@ targets=("x86_64-unknown-linux-gnu" "x86_64-pc-windows-gnu")
 
 for target in ${targets[@]}
 do
-    cargo build --release --target=$target
+    cargo build --release --target=$target 2>/dev/null
+    lastexitcode=$?
+    if [ $lastexitcode -eq 0 ]; then
+        echo "[+] $target compilation success"
+    else
+        echo "[-] compilation failed with code $lastexitcode. make sure $target is marked as \"installed\" in \"rustup target list\""
+    fi
 done
