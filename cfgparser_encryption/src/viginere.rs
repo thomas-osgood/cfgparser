0757b37baac3f5d90c35c3b5f5f6ae69ee6375e1
thomas-osgood <thomas-osgood@users.noreply.github.com>
2026-03-07 15:59:33 -0500

    added module comments

--- cfgparser_encryption/src/viginere.rs
+++ cfgparser_encryption/src/viginere.rs
@@ -1,1 +1,6 @@
+//! module designed to implement the vigenere cipher.
+//!
+//! this contains encrypt() and decrypt() functions along with
+//! helper functions that will be used in their logic.
+
 pub mod engine;
--- cfgparser_encryption/src/xor.rs
+++ cfgparser_encryption/src/xor.rs
@@ -1,1 +1,7 @@
+//! module designed to implement XOR "encryption".
+//!
+//! this contains a main encrypt_decrypt() function to perform the
+//! main XOR logic, along with helper functions that are used during
+//! the encrypt/decrypt logic.
+
 pub mod engine;
