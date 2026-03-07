2653f1c0b3921ec7084fb249365f8bdad917b776
thomas-osgood <thomas-osgood@users.noreply.github.com>
2026-03-07 02:21:30 -0500

    began lib comment

--- cfgparser_encryption/src/lib.rs
+++ cfgparser_encryption/src/lib.rs
@@ -1,2 +1,7 @@
+//! this is a library that contains simple encryption methods and functions.
+//!
+//! this is designed to be used by cfgparser_core for its main functionality
+//! and obfuscation.
+
 pub mod caesar;
 pub mod xor;
