ba4bc7e842524df5c45f2c74326a429ef1ebe0d6
thomas-osgood <thomas-osgood@users.noreply.github.com>
2026-03-08 20:28:48 -0400

    added default test

--- cfgparser_core/src/models/core/unit_tests.rs
+++ cfgparser_core/src/models/core/unit_tests.rs
@@ -1,8 +1,20 @@
 use super::*;
 
 #[test]
+/// test designed to confirm the Default implementation works as expected
+/// for the Configuration struct.
+fn test_configuration_struct_default() {
+    let expected: Configuration = Configuration {
+        host: "localhost".to_string(),
+        port: 80,
+    };
+    let test_struct: Configuration = Configuration::default();
+    assert_eq!(test_struct, expected);
+}
+
+#[test]
 /// test designed to confirm the serde default value works as expected.
-fn test_deserialize_default() {
+fn test_configuration_deserialize_default() {
     let test: &str = "{\"host\": \"localhost\"}";
     let expected: Configuration = Configuration {
         host: "localhost".to_string(),
