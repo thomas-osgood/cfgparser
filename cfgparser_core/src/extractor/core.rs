f32070b6872140c21c83131d4be8f85d747cc5e5
thomas-osgood <20747585+thomas-osgood@users.noreply.github.com>
2026-03-28 19:44:32 -0400

    feat(extractor): Read Bytes Generic Type (#16)
    * began defining CfgByteReader trait
    
    * added CfgByteReader impl for File and Cursor<Vec<u8>>
    
    * added Read and Seek generic to read_bytes
    
    * renamed input var for read_bytes
    
    * removed unneeded import
    
    * removed trait and impls
    
    * added read_bytes test
    
    * updated comment
    
    ---------
    
    Co-authored-by: thomas-osgood <thomas-osgood@users.noreply.github.com>

--- cfgparser_core/src/extractor/core.rs
+++ cfgparser_core/src/extractor/core.rs
@@ -1,4 +1,5 @@
-use std::io::{Read, Seek};
+#[cfg(test)]
+mod unit_tests;
 
 /// constant size of the byte buffer that will be used
 /// to hold the bytes representing the size of the
@@ -14,16 +15,20 @@ pub trait CfgExtractor {
 }
 
 /// generic function designed to read the configuration bytes from
-/// a file and return either a Vec<u8> or an error.
-fn read_bytes(mut fptr: std::fs::File) -> std::io::Result<Vec<u8>> {
+/// a reader that implements the Read + Seek traits and return
+/// either a Vec<u8> or an error.
+fn read_bytes<R>(mut reader: R) -> std::io::Result<Vec<u8>>
+where
+    R: std::io::Read + std::io::Seek,
+{
     let size_start: i64 = SZ_SIZEBUFF as i64 * -1;
 
     // allocate buffer that will hold the size bytes.
     let mut buf_sz: [u8; SZ_SIZEBUFF as usize] = [0; SZ_SIZEBUFF];
     // jumpt to the start of the size bytes (end - 8 bytes).
-    let _: u64 = fptr.seek(std::io::SeekFrom::End(size_start))?;
+    let _: u64 = reader.seek(std::io::SeekFrom::End(size_start))?;
 
-    fptr.read_exact(&mut buf_sz)?;
+    reader.read_exact(&mut buf_sz)?;
 
     // convert the bytes read into i64.
     let sz_payload: i64 = i64::from_be_bytes(buf_sz);
@@ -33,13 +38,13 @@ fn read_bytes(mut fptr: std::fs::File) -> std::io::Result<Vec<u8>> {
     let cfg_offset: i64 = (8 + sz_payload) * -1;
 
     // jump to the start of the config.
-    fptr.seek(std::io::SeekFrom::End(cfg_offset))?;
+    reader.seek(std::io::SeekFrom::End(cfg_offset))?;
 
     // allocate buffer for payload bytes.
     let mut payload_buf: Vec<u8> = vec![0u8; sz_payload as usize];
 
     // read the payload bytes into the payload buffer.
-    fptr.read_exact(&mut payload_buf)?;
+    reader.read_exact(&mut payload_buf)?;
 
     Ok(payload_buf)
 }
