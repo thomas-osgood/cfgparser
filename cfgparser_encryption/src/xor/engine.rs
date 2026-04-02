2443f59d2fb397a949a6a1f188b12c273b6def46
thomas-osgood <thomas-osgood@users.noreply.github.com>
2026-04-02 19:46:24 -0400

    began defining XORCipher

--- cfgparser_encryption/src/xor/engine.rs
+++ cfgparser_encryption/src/xor/engine.rs
@@ -1,6 +1,30 @@
 #[cfg(test)]
 mod unit_tests;
 
+#[derive(Default, Debug, Clone, PartialEq, Eq)]
+/// func designed to implement Decrypt and Encrypt for an XOR cipher.
+pub struct XORCipher {
+    pub key: Vec<u8>,
+}
+
+impl XORCipher {
+    pub fn new(key: Vec<u8>) -> XORCipher {
+        XORCipher { key }
+    }
+}
+
+impl crate::Decryptor for XORCipher {
+    fn decrypt(&self, ciphertext: Vec<u8>) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+        Ok(encrypt_decrypt(self.key.clone(), ciphertext))
+    }
+}
+
+impl crate::Encryptor for XORCipher {
+    fn encrypt(&self, plaintext: Vec<u8>) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+        Ok(encrypt_decrypt(self.key.clone(), plaintext))
+    }
+}
+
 /// function designed to XOR encrypt/decrypt data using a given key.
 pub fn encrypt_decrypt(key: Vec<u8>, data: Vec<u8>) -> Vec<u8> {
     let key_len: usize = key.len();
