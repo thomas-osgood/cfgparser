368063d442ffdc0d0c18423f4d6ab4c6234f8e08
thomas-osgood <20747585+thomas-osgood@users.noreply.github.com>
2026-04-02 22:27:17 -0400

    feat(encryption): Ecnryptor & Decryptor Traits (#29)
    * began defining Decryptor trait
    
    * updated Decryptor return type
    
    * added Encryptor trait definition
    
    * made ViginereCipher strut with Decryptor and Encryptor traits
    
    * updated Viginere new func
    
    * updated test_decrypt and test_encrypt functions
    
    * added derive to ViginereCipher struct
    
    * began defining XORCipher
    
    * minor comment update
    
    * added comment
    
    * updated comments
    
    * added test_xorcipher
    
    ---------
    
    Co-authored-by: thomas-osgood <thomas-osgood@users.noreply.github.com>

--- cfgparser_encryption/src/xor/engine.rs
+++ cfgparser_encryption/src/xor/engine.rs
@@ -1,6 +1,38 @@
 #[cfg(test)]
 mod unit_tests;
 
+#[derive(Default, Debug, Clone, PartialEq, Eq)]
+/// struct designed to implement Decrypt and Encrypt for an XOR cipher.
+pub struct XORCipher {
+    pub key: Vec<u8>,
+}
+
+impl XORCipher {
+    pub fn new(key: Vec<u8>) -> XORCipher {
+        XORCipher { key }
+    }
+}
+
+impl crate::Decryptor for XORCipher {
+    /// implementation of the Decryptor trait for XORCipher.
+    ///
+    /// this will call the encrypt_decrypt function with the
+    /// passed in ciphertext and the XORCipher's key.
+    fn decrypt(&self, ciphertext: Vec<u8>) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+        Ok(encrypt_decrypt(self.key.clone(), ciphertext))
+    }
+}
+
+impl crate::Encryptor for XORCipher {
+    /// implementation of the Encryptor trait for XORCipher.
+    ///
+    /// this will call the encrypt_decrypt function with the
+    /// passed in plaintext and the XORCipher's key.
+    fn encrypt(&self, plaintext: Vec<u8>) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+        Ok(encrypt_decrypt(self.key.clone(), plaintext))
+    }
+}
+
 /// function designed to XOR encrypt/decrypt data using a given key.
 pub fn encrypt_decrypt(key: Vec<u8>, data: Vec<u8>) -> Vec<u8> {
     let key_len: usize = key.len();
