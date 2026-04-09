80af3eb4a6cfe5c933e093a3317f01bcb7beacfe
thomas-osgood <thomas-osgood@users.noreply.github.com>
2026-04-08 23:02:17 -0400

    added test_bytesextractor_get_stream

--- cfgparser_core/src/extractor/core/unit_tests.rs
+++ cfgparser_core/src/extractor/core/unit_tests.rs
@@ -39,6 +39,32 @@ fn test_read_bytes() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 #[test]
+/// test designed to confirm the the BytesExtractor's `get_stream()` function
+/// returns the expected value.
+fn test_bytesextractor_get_stream() {
+    // extractor initialized with empty vector.
+    let mut extractor: BytesExtractor = BytesExtractor::new(vec![]);
+
+    // stream that will be passed into the set stream function.
+    let stream: Vec<u8> = vec![
+        116, 104, 105, 115, 32, 105, 115, 32, 97, 32, 98, 117, 110, 99, 104, 32, 111, 102, 32, 106,
+        117, 110, 107, 32, 100, 97, 116, 97, 32, 116, 104, 97, 116, 32, 105, 115, 32, 103, 111,
+        105, 110, 103, 32, 116, 111, 32, 98, 101, 32, 117, 115, 101, 100, 32, 116, 111, 32, 115,
+        112, 111, 111, 102, 32, 97, 32, 102, 105, 108, 101, 22, 24, 40, 29, 7, 64, 47, 82, 59, 15,
+        28, 6, 43, 31, 84, 27, 3, 42, 60, 9, 16, 15, 56, 30, 6, 26, 40, 17, 59, 38, 57, 22, 0, 65,
+        47, 67, 40, 8, 29, 2, 60, 53, 9, 71, 42, 32, 22, 5, 59, 11, 61, 11, 3, 53, 51, 7, 59, 49,
+        59, 83, 58, 34, 40, 29, 1, 59, 51, 21, 17, 28, 57, 88, 0, 0, 0, 0, 0, 0, 0, 72,
+    ];
+
+    // assign the stream to the extractor.
+    extractor.set_stream(stream.clone());
+
+    // confirm the `get_stream` return is the matches the vector
+    // that was passed into `set_stream`.
+    assert_eq!(stream, extractor.get_stream());
+}
+
+#[test]
 /// test designed to confirm the expected functionality of
 /// the `set_stream()` function of the BytesExtractor.
 fn test_bytesextractor_set_stream() -> Result<(), Box<dyn std::error::Error>> {
