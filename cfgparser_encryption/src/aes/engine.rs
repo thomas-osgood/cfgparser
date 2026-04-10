2194d02f120900212a333bdabd5199be057f1c42
thomas-osgood <thomas-osgood@users.noreply.github.com>
2026-04-09 23:02:22 -0400

    began defining impl for AESCipher

--- cfgparser_encryption/src/aes/engine.rs
+++ cfgparser_encryption/src/aes/engine.rs
@@ -2,3 +2,15 @@
 pub struct AESCipher {
     key: Vec<u8>,
 }
+
+/// function designed to check whether a given AES key is valid.
+fn validate_key(key: Vec<u8>) -> Result<(), Box<dyn std::error::Error>> {
+    Ok(())
+}
+
+impl AESCipher {
+    pub fn new(key: Vec<u8>) -> Result<AESCipher, Box<dyn std::error::Error>> {
+        validate_key(key.clone())?;
+        Ok(AESCipher { key })
+    }
+}
