c6cd922d2444c8a370038954ef8e09c2ae1e95b0
thomas-osgood <thomas-osgood@users.noreply.github.com>
2026-04-09 23:08:40 -0400

    began custom error enum. updated validat_key logic.

--- cfgparser_encryption/src/aes/engine.rs
+++ cfgparser_encryption/src/aes/engine.rs
@@ -1,15 +1,29 @@
+#[derive(Debug)]
+pub enum AESError {
+    KeyLength(String),
+}
+
+const ERR_KEY_LEN: &str = "invalid key length. must be 16, 24 or 32 bytes";
+
 #[derive(Clone, Debug, Default, PartialEq, Eq)]
 pub struct AESCipher {
     key: Vec<u8>,
 }
 
 /// function designed to check whether a given AES key is valid.
-fn validate_key(key: Vec<u8>) -> Result<(), Box<dyn std::error::Error>> {
+fn validate_key(key: Vec<u8>) -> Result<(), AESError> {
+    let valid_lengths: Vec<usize> = vec![16, 24, 32];
+    let key_size: usize = key.len();
+
+    if !valid_lengths.contains(&key_size) {
+        return Err(AESError::KeyLength(ERR_KEY_LEN.to_string()));
+    }
+
     Ok(())
 }
 
 impl AESCipher {
-    pub fn new(key: Vec<u8>) -> Result<AESCipher, Box<dyn std::error::Error>> {
+    pub fn new(key: Vec<u8>) -> Result<AESCipher, AESError> {
         validate_key(key.clone())?;
         Ok(AESCipher { key })
     }
