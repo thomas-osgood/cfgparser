edef99312cd31ddff7daab2e2c686c064b31eed6
thomas-osgood <thomas-osgood@users.noreply.github.com>
2026-04-12 19:49:25 -0400

    began initial cfgparser header file

---
+++ helpers/c/cfgparser.h
@@ -0,0 +1,42 @@
+#ifndef __CFGPARSER_H
+#define __CFGPARSER_H
+
+/*
+function designed to read the end of the current binary and
+extract configuration information from it. this will return
+the C2 address in the form "<scheme>://<ip>:<port>".
+
+the extracted bytes are XOR-decrypted and base64-decoded before
+being JSON deserialized into a configuration struct. the information
+in this struct is then used to build the string that gets
+returned by the function.
+
+note: if the key passed in is NULL, a default of 'q' will be used
+when attempting to decrypt the bytes.
+*/
+extern char *read_cfg(char *key);
+
+/*
+function designed to read the end of a target file and
+extract configuration information from it. this will return
+the C2 address in the form "<scheme>://<ip>:<port>".
+
+the extracted bytes are XOR-decrypted and base64-decoded before
+being JSON deserialized into a configuration struct. the information
+in this struct is then used to build the string that gets
+returned by the function.
+
+note: if the key passed in is NULL, a default of 'q' will be used
+when attempting to decrypt the bytes.
+*/
+extern char *read_cfg_from_file(char *filename, char *key);
+
+/*
+function designed to free memory that was originally allocated
+by rust. since the pointer was created by the rust library, the
+rust library still owns that memory and, therefore, must be freed
+by the library.
+*/
+extern void free_memory(char *ptr);
+
+#endif // __CFGPARSER_H
