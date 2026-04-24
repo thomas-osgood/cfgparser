f53f1b959d672f141cb192e275917fb1250ac2ef
thomas-osgood <thomas-osgood@users.noreply.github.com>
2026-04-23 23:07:12 -0400

    added read_cfg_from_file_with_encryption_o

--- helpers/c/cfgparser.h
+++ helpers/c/cfgparser.h
@@ -75,6 +75,23 @@ extern "C"
     extern char *read_cfg_from_file_with_encryption(char *filename, char *key, int enc_type);
 
     /*
+    function designed to read the end of a target file and
+    extract configuration information from it. this will return
+    the C2 address in the form "<scheme>://<ip>:<port>".
+
+    the extracted bytes are decrypted and base64-decoded before
+    being JSON deserialized into a configuration struct. the information
+    in this struct is then used to build the string that gets
+    returned by the function.
+
+    the type of encryption is specified by the "enc_type" argument.
+
+    the offset specifies how far back to search from the end of the file
+    to find the end of the size block of the configuration.
+    */
+    extern char *read_cfg_from_file_with_encryption_o(char *filename, char *key, int enc_type, unsigned int offset);
+
+    /*
     function designed to free memory that was originally allocated
     by rust. since the pointer was created by the rust library, the
     rust library still owns that memory and, therefore, must be freed
